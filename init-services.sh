7da8e854455098a700b309e7a81b72d57f293c2b
Jonathan Wiesel <jonathanwiesel@gmail.com>
2014-06-05 15:49:50 -0430

    Add correct mon services initalization

--- init-services.sh
+++ init-services.sh
@@ -1,7 +1,5 @@
 #! /usr/bin/env bash
 
-/usr/sbin/cron -f &
-
-/usr/sbin/sshd -D &
-
-/usr/sbin/nginx -c /etc/nginx/nginx.conf
+mon -d "/usr/sbin/nginx -c /etc/nginx/nginx.conf" -p /home/codehero/processes/pids/nginx.pid -l /home/codehero/processes/logs/nginx.log -E './home/codehero/slack_message.sh "Nginx has exited with error"' -R './home/codehero/slack_message.sh "Nginx has been restarted"'
+mon -d "/usr/sbin/cron -f" -p /home/codehero/processes/pids/cron.pid -l /home/codehero/processes/logs/cron.log -E './home/codehero/slack_message.sh "Cron has exited with error"' -R './home/codehero/slack_message.sh "Cron has been restarted"'
+mon -d "/usr/sbin/sshd -D" -p /home/codehero/processes/pids/sshd.pid -l /home/codehero/processes/logs/sshd.log -E './home/codehero/slack_message.sh "SSHD has exited with error"' -R './home/codehero/slack_message.sh "SSHD has been restarted"'
