7325f2457d4ded538713cdd124004f5c82ec3b34
Jonathan Wiesel <jonathanwiesel@gmail.com>
2015-02-08 00:14:47 -0430

    Give more ownership to cohere user
    Avoid future breaking

--- server_config.sh
+++ server_config.sh
@@ -19,8 +19,20 @@ chmod +x /home/codehero/build-script.sh
 # add the crontab script to crontab
 crontab -u codehero /home/codehero/crontab-script.sh
 
+# create publish directory
+mkdir /var/www/codehero-jekyll
+
+# make codehero owner of publish directory
+chown -R codehero:codehero /var/www/codehero-jekyll
+
 # make codehero owner of its home
 chown -R codehero:codehero /home/codehero
+
+# make codehero owner of /usr/local so it can install gems on default location
+chown -R codehero:codehero /usr/local
+
+# give back /usr/local/sbin to root, this should always belong to root
+chown -R root:root /usr/local/sbin
 
 # move and apply the nginx configuration
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
