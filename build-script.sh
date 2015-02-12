67e2f8c1fe7e366617c65d597b5222a21e995f35
Jonathan Wiesel <jonathanwiesel@gmail.com>
2015-02-12 17:48:43 -0430

    Fix change control strategy

--- build-script.sh
+++ build-script.sh
@@ -8,25 +8,25 @@ PUBLIC_WWW=/var/www/codehero-jekyll
 
 BUNDLE="/usr/local/bin/bundle"
 
+LOCAL_REPO_HASH=$(git -C $GIT_REPO rev-parse @{0})
+REMOTE_REPO_HASH=$(git -C $GIT_REPO rev-parse @{u})
+ISNEW=false
+
 if [ ! -d "$GIT_REPO" ]; then
   echo "--------------> Cloning blog repo"
   git clone $REMOTE_REPO $GIT_REPO
+  ISNEW=true
 fi
 
-echo "--------------> Pulling changes latest changes..."
-git -C "$GIT_REPO" pull origin master
+if [ LOCAL_REPO_HASH != REMOTE_REPO_HASH ] || ISNEW ; then
+  echo "--------------> Pulling changes latest changes..."
+  git -C "$GIT_REPO" pull origin master
 
-# Check if any file has changed, if so rebuild the blog
-NEW_HASH=$(ls -laR $GIT_REPO | md5sum)
-
-if [ "$NEW_HASH" != "$REPO_HASH" ]; then
   echo "--------------> Starting to build the blog"
   echo "--------------> Installing dependencies if needed"
   $BUNDLE install --gemfile "$GIT_REPO"/Gemfile --deployment --without test --without development
   BUNDLE_GEMFILE="$GIT_REPO"/Gemfile $BUNDLE exec jekyll build --source $GIT_REPO --destination $PUBLIC_WWW
   echo "--------------> Application Deployed to $PUBLIC_WWW"
-
-  export REPO_HASH="$NEW_HASH"
 else
   echo "--------------> Blog is up to date, doesn't need regeneration"
 fi
